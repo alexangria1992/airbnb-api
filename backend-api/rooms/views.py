ce4a296e19a4e97533f0994adae82523feae9486
alexangria1992 <alex040892@gmail.com>
2022-03-21 20:47:54 +1300

    Serializers

--- backend-api/rooms/views.py
+++ backend-api/rooms/views.py
@@ -1,9 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .models import Room
+from .serializers import RoomSerializer
 # Create your views here.
 
 
 @api_view(["GET", "DELETE"])
 def list_rooms(request):
-    return Response()
+    rooms = Room.objects.all()
+    serialized_rooms = RoomSerializer(rooms, many=True)
+    return Response(data=serialized_rooms.data)
