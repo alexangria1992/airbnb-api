0aec5b4ba96b4c6060bd52ab1c2c9f3a6f6fc6aa
alexangria1992 <alex040892@gmail.com>
2022-03-21 22:20:50 +1300

    ModelViewSet

--- backend-api/rooms/urls.py
+++ backend-api/rooms/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from . import viewsets
+from rest_framework.routers import DefaultRouter
+
 app_name = "rooms"
 
-urlpatterns = [path("list/", views.ListRoomsView.as_view()),
-               path("<int:pk>/", views.SeeRoomView.as_view())]
+router = DefaultRouter()
+router.register("", viewsets.RoomViewset, basename="room")
+
+
+urlpatterns = router.urls
