2102a7478cc77e5066295a74cfadc03986a777c7
alexangria1992 <alex040892@gmail.com>
2022-03-21 21:42:36 +1300

    ListAPIView

--- backend-api/rooms/urls.py
+++ backend-api/rooms/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 app_name = "rooms"
 
-urlpatterns = [path("list/", views.ListRoomsView.as_view())]
+urlpatterns = [path("list/", views.ListRoomsView.as_view()),
+               path("<int:pk>/", views.SeeRoomView.as_view())]
